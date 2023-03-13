5595a83a1fed8ff1e87e0f296e3653aed837ea51
fathantsaqib <fathants12@gmail.com>
2023-03-13 17:46:44 +0800

    kumpulprak1

---
+++ H071221099/Pertemuan-1/No2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class No2 {
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        try {
+            System.out.print("Masukkan jumlah bilangan: ");
+            int number = inp.nextInt();
+
+            int desimal = 0;
+            int bulat = 0;
+
+            for (int i = 0; i < number; i++) {
+                // Arti dari perulangan ini adalah "untuk nilai awal i sama dengan 0; lakukan
+                // perintah di dalam blok perulangan ini selama i kurang dari nilai n;
+                // setiap kali perintah-perintah di dalam blok perulangan selesai dieksekusi,
+                // tambahkan 1 ke nilai i".
+                // Dalam konteks ini, n adalah sebuah variabel atau konstanta yang menyatakan
+                // jumlah iterasi atau pengulangan yang ingin dilakukan.
+
+                // System.out.print("Masukkan bilangan ke-" + (i + 1) + ": ");
+                double ngka = inp.nextDouble();
+
+                if (ngka % 1 == 0) {
+                    bulat++;
+                } else {
+                    desimal++;
+                }
+            }
+
+            System.out.println(bulat + " Bilangan Bulat");
+            System.out.println(desimal + " Bilangan Desimal");
+            inp.close();
+        } catch (Exception e) {
+            System.out.println("Hanya bisa menerima angka");
+        }
+    }
+}
---
+++ H071221099/Pertemuan-1/No5.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class No5 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        String hari, angkaBulan, tahun, tanggal, angkaTahun;
+        String namaBulan="";
+        System.out.print("Input: ");
+        tanggal = inp.nextLine();
+        hari = tanggal.substring(0, 1);
+        angkaBulan = tanggal.substring(3, 5);
+        tahun = tanggal.substring(6, 8);
+
+        if (Integer.parseInt(tahun) > 23) {
+            angkaTahun = "19"+tahun;
+        }else {
+            angkaTahun = "20"+tahun;
+        }
+        inp.close();
+        
+        
+        switch (Integer.parseInt(angkaBulan)) {
+            case 1:
+                namaBulan = "Januari";
+                break;
+            case 2:
+                namaBulan = "Februari";
+                break;
+            case 3:
+                namaBulan = "Maret";
+                break;
+            case 4:
+                namaBulan = "April";
+                break;
+            case 5:
+                namaBulan = "Mei";
+                break;
+            case 6:
+                namaBulan = "Juni";
+                break;
+            case 7:
+                namaBulan = "July";
+                break;
+            case 8:
+                namaBulan = "Agustus";
+                break;
+            case 9:
+                namaBulan = "September";
+                break;   
+            case 10:
+                namaBulan = "Oktober";
+                break; 
+            case 11:
+                namaBulan = "November";
+                break;
+            case 12:
+                namaBulan = "Desember";
+                break;
+            default:
+                System.out.println("Angka yang dimasukkan tidak valid");
+                break;
+        }
+        System.out.println(Integer.parseInt(hari)+" "+namaBulan+" "+angkaTahun);
+    }
+}
