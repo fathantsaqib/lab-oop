d05cd6cec068f087dc374bb7b7eef2b0ec160940
Raflidahlan17 <muhrafli1711@gmail.com>
2023-03-13 19:41:35 +0800

    kumpul tugas praktikum

---
+++ H071221104/Pertemuan-1/No1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class No1 {
+    public static void main(String[] args) {
+        int nim;
+        Scanner key = new Scanner(System.in);
+        System.out.print("Masukkan angka terakhir NIM: ");
+        nim = key.nextInt();
+        if (nim % 7 == 0){System.out.print("Soal no:7");}
+        else {System.out.println("soal nomor :"+ nim % 7);}
+        // else if (nim % 7 == 1){System.out.print("Soal no:1");}
+        // else if(nim % 7 == 2){System.out.print("Soal no:2");}
+        // else if(nim % 7 == 3){System.out.print("Soal no:3");}
+        // else if(nim % 7 == 4){System.out.print("Soal no:4");}
+        // else if(nim % 7 == 5){System.out.print("Soal no:5");}
+        // else if(nim % 7 == 6){System.out.print("Soal no:6");}
+
+        key.close();
+    }
+}
