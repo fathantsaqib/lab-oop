6daedb344f7b0ec8e74a7ecccc7ce414aa42e64c
Fadillah Cheryl <fadillahcheryl@serls-MacBook-Air.local>
2023-03-13 14:27:54 +0800

    Tugas Praktikum 1

---
+++ H071221077/Pertemuan-1/assignment_1_1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class no1 {
+    public static void main(String[] args) {
+        String nim;
+        Scanner keyboard = new Scanner(System.in);      // untuk menginput
+        System.out.print("Masukkan NIM: ");
+        nim = keyboard.next();      //tipe data
+        String inp = nim.substring(nim.length()-3);     // inputan utk mengambil 3 angka dblkg nim
+        
+        int angka = Integer.parseInt(inp);      //mengubah str menjadi int
+
+        if (angka % 7 == 0) {
+            System.out.println("Soal: no 7");       // kalau angka % 7 = 0 muncul no 7
+            } else{
+                System.out.printf("Soal : no %d\n" , angka % 7 );       //kalau angka % 7 = selain 0, muncul
+            } 
+        
+        keyboard.close();
+    }
+
+}
