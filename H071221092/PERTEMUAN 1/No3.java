cf9a5fdeb506cbfa8a68a048b1b4d4fb5b46ac0d
fara1201 <114276188+fara1201@users.noreply.github.com>
2023-03-13 15:10:53 +0800

    Kumpul tugas 1

---
+++ H071221092/PERTEMUAN 1/No3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class No3 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner (System.in);
+
+        try{
+            System.out.print("Name  : ");
+            String name = input.nextLine();
+
+            System.out.print("Age   : ");
+            int age = input.nextInt();
+            input.nextLine();  //baris 
+
+            System.out.print("Hobby : ");  
+            String hobby = input.nextLine();
+
+            System.out.print("Nama saya " + name + ", " + age + " Tahun, hobby " + hobby + ".");
+        } catch (Exception e) {
+            System.out.println("Umur harus berupa angka");
+        }
+        input.close() ;
+    }
+}
---
+++ H071221092/PERTEMUAN 1/No8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class No8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input satu bilangan
+        try {
+            System.out.print("Masukkan satu bilangan : ");
+            int bil = scanner.nextInt();
+        
+
+        int[][] array2D = {{1,2,3}, {4,5,6}, {7,8,9}};
+        boolean found = false;
+
+        for (int i = 0; i < array2D.length; i++) {
+            for (int j = 0; j < array2D[i].length; j++) { 
+                if (array2D[i][j] == bil) {
+                    System.out.println("Bilangan " + bil + " ditemukan pada indeks [" + i + "][" + j + "]");
+                    found = true;
+                    return;
+                }
+            }
+        }
+        if (!found) {
+            System.out.println("Bilangan " + bil + " tidak ditemukan pada array 2D");
+        } 
+        } catch (Exception e) {
+            System.out.println("Inputan harus berupa angka");
+
+        }
+        scanner.close();
+    }
+ }
