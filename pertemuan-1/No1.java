c09a2611c6fe93ff9e54ce2ce7cd008d947ed260
Nadjwa03 <nadjwajufri03@gmail.com>
2023-03-13 20:43:26 +0800

    praktikum tugas 1

---
+++ pertemuan-1/No1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class No1 { // nama file classnya
+    public static void main(String[] args) {
+        String nim;                               // variable
+        Scanner keyboard = new Scanner(System.in);  
+        System.out.print("Masukkan NIM: ");
+        nim = keyboard.next();
+        String inp = nim.substring(nim.length()- 3); // memasukkan index yang diminta, -3 untuk 3 angka terakkhir
+
+        int angka = Integer.parseInt(inp); // modifikasi tipedata
+
+        if (angka % 7 == 0) {
+            System.out.println("soal: no 7");
+        } else {
+            System.out.printf("soal : no %d\n", angka % 7); //%d untuk angka int
+        }
+
+       keyboard.close();
+    }
+
+}
